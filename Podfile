c6b997bef912d0c0f07529dc3e0bdc6ee4b22310
Marc Audefroy <audefroy.marc@gmail.com>
2014-02-22 09:28:44 +0100

    init project

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, "6.1"
+inhibit_all_warnings!
+
+
+pod 'MTDates', '~> 0.11'
+pod 'AFNetworking' , '~> 2.0.1'
+pod 'OHAutoNIBi18n' , '~> 0.1.0'
+pod 'MMDrawerController'
+pod 'MMDrawerController+Storyboard'
+pod 'MagicalRecord/Shorthand', '~> 2.2'
+pod 'BlockRSSParser', '~> 2.1'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,110 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// AFNetworking
+#define COCOAPODS_POD_AVAILABLE_AFNetworking
+#define COCOAPODS_VERSION_MAJOR_AFNetworking 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking 3
+
+// AFNetworking/NSURLConnection
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLConnection
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLConnection 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLConnection 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLConnection 3
+
+// AFNetworking/NSURLSession
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_NSURLSession
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_NSURLSession 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_NSURLSession 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_NSURLSession 3
+
+// AFNetworking/Reachability
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Reachability
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Reachability 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Reachability 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Reachability 3
+
+// AFNetworking/Security
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Security
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Security 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Security 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Security 3
+
+// AFNetworking/Serialization
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_Serialization
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_Serialization 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_Serialization 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_Serialization 3
+
+// AFNetworking/UIKit
+#define COCOAPODS_POD_AVAILABLE_AFNetworking_UIKit
+#define COCOAPODS_VERSION_MAJOR_AFNetworking_UIKit 2
+#define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 0
+#define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 3
+
+// BlockRSSParser
+#define COCOAPODS_POD_AVAILABLE_BlockRSSParser
+#define COCOAPODS_VERSION_MAJOR_BlockRSSParser 2
+#define COCOAPODS_VERSION_MINOR_BlockRSSParser 1
+#define COCOAPODS_VERSION_PATCH_BlockRSSParser 0
+
+// MMDrawerController
+#define COCOAPODS_POD_AVAILABLE_MMDrawerController
+#define COCOAPODS_VERSION_MAJOR_MMDrawerController 0
+#define COCOAPODS_VERSION_MINOR_MMDrawerController 5
+#define COCOAPODS_VERSION_PATCH_MMDrawerController 3
+
+// MMDrawerController/Core
+#define COCOAPODS_POD_AVAILABLE_MMDrawerController_Core
+#define COCOAPODS_VERSION_MAJOR_MMDrawerController_Core 0
+#define COCOAPODS_VERSION_MINOR_MMDrawerController_Core 5
+#define COCOAPODS_VERSION_PATCH_MMDrawerController_Core 3
+
+// MMDrawerController/MMDrawerBarButtonItem
+#define COCOAPODS_POD_AVAILABLE_MMDrawerController_MMDrawerBarButtonItem
+#define COCOAPODS_VERSION_MAJOR_MMDrawerController_MMDrawerBarButtonItem 0
+#define COCOAPODS_VERSION_MINOR_MMDrawerController_MMDrawerBarButtonItem 5
+#define COCOAPODS_VERSION_PATCH_MMDrawerController_MMDrawerBarButtonItem 3
+
+// MMDrawerController/MMDrawerVisualStates
+#define COCOAPODS_POD_AVAILABLE_MMDrawerController_MMDrawerVisualStates
+#define COCOAPODS_VERSION_MAJOR_MMDrawerController_MMDrawerVisualStates 0
+#define COCOAPODS_VERSION_MINOR_MMDrawerController_MMDrawerVisualStates 5
+#define COCOAPODS_VERSION_PATCH_MMDrawerController_MMDrawerVisualStates 3
+
+// MMDrawerController/Subclass
+#define COCOAPODS_POD_AVAILABLE_MMDrawerController_Subclass
+#define COCOAPODS_VERSION_MAJOR_MMDrawerController_Subclass 0
+#define COCOAPODS_VERSION_MINOR_MMDrawerController_Subclass 5
+#define COCOAPODS_VERSION_PATCH_MMDrawerController_Subclass 3
+
+// MMDrawerController+Storyboard
+#define COCOAPODS_POD_AVAILABLE_MMDrawerController_Storyboard
+#define COCOAPODS_VERSION_MAJOR_MMDrawerController_Storyboard 0
+#define COCOAPODS_VERSION_MINOR_MMDrawerController_Storyboard 0
+#define COCOAPODS_VERSION_PATCH_MMDrawerController_Storyboard 1
+
+// MTDates
+#define COCOAPODS_POD_AVAILABLE_MTDates
+#define COCOAPODS_VERSION_MAJOR_MTDates 0
+#define COCOAPODS_VERSION_MINOR_MTDates 11
+#define COCOAPODS_VERSION_PATCH_MTDates 0
+
+// MagicalRecord/Shorthand
+#define COCOAPODS_POD_AVAILABLE_MagicalRecord_Shorthand
+#define COCOAPODS_VERSION_MAJOR_MagicalRecord_Shorthand 2
+#define COCOAPODS_VERSION_MINOR_MagicalRecord_Shorthand 2
+#define COCOAPODS_VERSION_PATCH_MagicalRecord_Shorthand 0
+
+// OHAutoNIBi18n
+#define COCOAPODS_POD_AVAILABLE_OHAutoNIBi18n
+#define COCOAPODS_VERSION_MAJOR_OHAutoNIBi18n 0
+#define COCOAPODS_VERSION_MINOR_OHAutoNIBi18n 1
+#define COCOAPODS_VERSION_PATCH_OHAutoNIBi18n 1
+
