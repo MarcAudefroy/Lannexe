944b58589732074fcfcc41e5cdf013a12f1ce713
Marc Audefroy <audefroy.marc@gmail.com>
2014-02-22 09:14:03 +0100

    Initial Commit

---
+++ Lannexe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lannexe
+//
+//  Created by Marc Audefroy on 22/02/14.
+//  Copyright (c) 2014 Supinfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LASAppDelegate class]));
+    }
+}
